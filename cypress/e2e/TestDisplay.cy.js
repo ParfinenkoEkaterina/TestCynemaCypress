256cc8e894e118abb58d4ff542670c8148b66021
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2024-01-24 17:14:39 +0300

    jenkins paralell

--- cypress/e2e/TestDisplay.cy.js
+++ cypress/e2e/TestDisplay.cy.js
@@ -12,7 +12,7 @@ tests.forEach((test) => {
         tests.forEach((test) => {
             it("Should be possible to book", () => {
                 cy.get('.page-nav > :nth-child(2)').click()
-                cy.get(':nth-child(2) > .movie-seances__time').first().contains('12:00').click();
+                cy.get(':nth-child(2) > .movie-seances__list').first().contains('12:00').click();
                 test.data.forEach((seat) => {
                 cy.get(`.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`).click();  
             })
